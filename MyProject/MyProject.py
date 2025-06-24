d51b4580fd160a2bbbd1830017e8a924748b3a01
anastasia-2107 <anastasia2107@inbox.ru>
2025-06-24 20:37:22 +0300

    24.06_

---
+++ MyProject/MyProject.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, request, flash, g
+import sqlite3
+import os
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'kjdfkreg5f4hf4s5f4gn8f7gn45xf4b5d4n5fg'
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = 'ba92ab4e8bfb8755797efef56a86d6698e91db0ffc'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, "flsk.db")))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource("sq_db.sql", "r") as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.get_menu())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    return render_template('add_post.html', menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+# menu = [
+#     {"name": "Главная", "url": "index"},
+#     {"name": "О нас", "url": "about"},
+#     {"name": "Услуги", "url": "services"},
+#     {"name": "Наши контакты", "url": "address"},
+#     {"name": "Обратная связь", "url": "contact"}
+# ]
+#
+#
+# @app.route("/index")
+# def index():
+#     return render_template("index.html", title="Главная", menu=menu)
+#
+#
+# @app.route("/about")
+# def about():
+#     return render_template("about.html", title="О нас", menu=menu)
+#
+# @app.route("/services")
+# def services():
+#     return render_template("services.html", title="Услуги", menu=menu)
+#
+# @app.route("/address")
+# def address():
+#     return render_template("address.html", title="Наши контакты", menu=menu)
+#
+# @app.route("/contact", methods=["POST", "GET"])
+# def contact():
+#     if request.method == "POST":
+#         # print(request.form)
+#         # print(request.form['username'])
+#         if len(request.form['username']) > 2:
+#             flash("Сообщение отправлено успешно!", category='success')
+#         else:
+#             flash("Ошибка отправки", category='error')
+#     return render_template("contact.html", title="Обратная связь", menu=menu)
+# # int - должны присутствовать только цифры
+# # float - может записываться число с плавающей точкой
+# # path - можно использовать любые допустимые символы URL плюс символ слеша("/")
+#
+# @app.route("/profile/<path:username>")
+# def profile(username):
+#     return f"Пользователь: {username}"
+#
+#
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template("page404.html", title="Страница не найдена", menu=menu)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
