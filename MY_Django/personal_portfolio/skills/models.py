66636f90d8dbd1c63c8ef00a330d0c2568cf5dc6
anastasia-2107 <anastasia2107@inbox.ru>
2025-07-14 22:35:36 +0300

    14.07

--- MY_Django/personal_portfolio/skills/models.py
+++ MY_Django/personal_portfolio/skills/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Skills(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField('skills/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
