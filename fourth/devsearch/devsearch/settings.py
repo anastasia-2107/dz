e83b334568528ea564c2b44f1191f106832a3764
anastasia-2107 <anastasia2107@inbox.ru>
2025-08-26 22:28:24 +0300

    Alex_Davydova1

--- fourth/devsearch/devsearch/settings.py
+++ fourth/devsearch/devsearch/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- fourth/devsearch/projects/views.py
+++ fourth/devsearch/projects/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
 
 
 def projects(request):
-    return render(request, "projects/projects.html")
+    pr = Project.objects.all()
+    contex = {
+        'projects': pr
+    }
+    return render(request, "projects/projects.html", contex)
+
+
+def project(request, pk):
+    project_obj = Project.objects.get(id=pk)
+    return render(request, "projects/single-project.html", {'project': project_obj})
+
+
+@login_required(login_url="login")
+def create_project(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('projects')
+
+    return render(request, 'projects/form-template.html', {'form': form})
